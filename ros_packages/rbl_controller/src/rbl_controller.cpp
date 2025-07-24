0382484fee3e108e58ffc446219e3200996d0e5f
mrs_gigabedna <vivaldini@ufscar.br>
2025-07-24 14:35:56 +0200

    updated: target distance

--- ros_packages/rbl_controller/src/rbl_controller.cpp
+++ ros_packages/rbl_controller/src/rbl_controller.cpp
@@ -1472,7 +1472,7 @@ void RBLController::goalUpdateLoop(const ros::TimerEvent&) {
     }
 
     // Step 2: Walk forward along the path and accumulate distance
-    const double target_distance = 5.0;
+    const double target_distance = 3.5;
     double accumulated_distance = 0.0;
     size_t best_idx = start_idx;
 
